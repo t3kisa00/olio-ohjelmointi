14882c295b6ede02423e5d5c8b9d4b71f818adfe
Remmy asd <t3kisa00@students.oamk.fi>
2024-11-13 02:00:08 +0200

    Lisätty binomikertoimen laskeminen

---
+++ README.md
@@ -0,0 +1,13 @@
+# Olio-ohjelmoinnin esimerkkitehtäviä
+
+## Esim1
+
+Demosin Gitin käyttöä
+
+## Esim2
+
+Esimerkkejä funktioista ja header-/cpp-tiedostojen käytöstä
+
+## Esim 3
+
+Kertoman ja binomikertoimen laskeminen
--- esim3/main.cpp
+++ esim3/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int num;
     int result;
+    int n, k;
     cout << "Lasketaan kertoma!" << endl;
     cout << "Anna positiivinen kokonaisluku" << endl;
     cin >> num;
@@ -14,5 +15,13 @@ int main() {
     } else {
     cout << "Luvun " << num << "kertoma = " << result << endl;
     }
+
+    cout<<"Anna n "<<endl;
+    cin>>n;
+    cout<<"Anna k"<<endl;
+    cin>>k;
+    result=coefficient(n,k);
+    cout<<" yli "<<k<<" = "<<result<<endl;
+
     return 0;
 }
